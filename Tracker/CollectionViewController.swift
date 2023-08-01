ec9dd4380f8475cf18cf5018519acb0d6a32a9ef
Anton Kashnikov <kashnikoff@bk.ru>
2023-08-01 23:24:48 +0200

    Added the window and collection view controller as a root view controller

--- Tracker/CollectionViewController.swift
+++ Tracker/CollectionViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CollectionViewController.swift
 //  Tracker
 //
 //  Created by Антон Кашников on 28.07.2023.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CollectionViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
--- Tracker/SceneDelegate.swift
+++ Tracker/SceneDelegate.swift
@@ -11,8 +11,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else {
+        guard let windowScene = scene as? UIWindowScene else {
             return
         }
+        
+        window = UIWindow(windowScene: windowScene)
+        let navigationController = UINavigationController(rootViewController: CollectionViewController())
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
     }
 }
