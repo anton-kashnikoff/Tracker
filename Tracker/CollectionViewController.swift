207a899a9ff52cf12a46e7a25b117911466865ec
Anton Kashnikov <kashnikoff@bk.ru>
2023-08-01 23:36:31 +0200

    Added base code for working with collection view

--- Tracker/CollectionViewController.swift
+++ Tracker/CollectionViewController.swift
@@ -7,8 +7,39 @@
 
 import UIKit
 
-class CollectionViewController: UIViewController {
+final class CollectionViewController: UIViewController {
+    var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        return collectionView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+        
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(collectionView)
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+}
+
+extension CollectionViewController: UICollectionViewDelegate {
+    
+}
+
+extension CollectionViewController: UICollectionViewDelegateFlowLayout {
+    
+}
+
+extension CollectionViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
     }
 }
