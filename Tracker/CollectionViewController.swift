3fcf98bf9281051fbc85e23f94053e90daf02289
Anton Kashnikov <kashnikoff@bk.ru>
2023-08-02 21:23:16 +0200

    Realized basic layout and interface of the main screen

--- Tracker/CollectionViewController.swift
+++ Tracker/CollectionViewController.swift
@@ -8,9 +8,48 @@
 import UIKit
 
 final class CollectionViewController: UIViewController {
-    var collectionView: UICollectionView = {
+    let collectionView: UICollectionView = {
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
         return collectionView
+    }()
+    
+    let barButtonItem: UIBarButtonItem = {
+        let barButtonItem = UIBarButtonItem()
+        barButtonItem.image = UIImage(named: "Add tracker icon")
+        barButtonItem.tintColor = .black
+        barButtonItem.action = #selector(addTracker)
+        return barButtonItem
+    }()
+    
+    let datePickerItem: UIBarButtonItem = {
+        let datePicker = UIDatePicker()
+        datePicker.preferredDatePickerStyle = .compact
+        datePicker.datePickerMode = .date
+        
+        let barButtonItem = UIBarButtonItem(customView: datePicker)
+        return barButtonItem
+    }()
+    
+    let searchTextField: UISearchTextField = {
+        let searchTextField = UISearchTextField()
+        searchTextField.placeholder = "Поиск"
+        searchTextField.backgroundColor = UIColor(red: 118, green: 118, blue: 128, alpha: 0.12)
+        searchTextField.translatesAutoresizingMaskIntoConstraints = false
+        return searchTextField
+    }()
+    
+    let imageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "star"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let label: UILabel = {
+        let label = UILabel()
+        label.text = "Что будем отслеживать?"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
     }()
     
     override func viewDidLoad() {
@@ -18,53 +57,60 @@ final class CollectionViewController: UIViewController {
         
         view.backgroundColor = .systemBackground
         
-        collectionView.translatesAutoresizingMaskIntoConstraints = false
+//        setupCollectionView()
+        setupNavigationBar()
+        setupSearchTextField()
+        setupStarImage()
+        setupLabel()
+    }
+    
+    private func setupCollectionView() {
         view.addSubview(collectionView)
-        
         collectionView.delegate = self
         collectionView.dataSource = self
         
-        setupNavigationBar()
-        setupSearchTextField()
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
     }
     
     private func setupNavigationBar() {
         title = "Трекеры"
         navigationController?.navigationBar.prefersLargeTitles = true
         
-        navigationItem.leftBarButtonItem = getAddTrackerButtonItem()
-        navigationItem.rightBarButtonItem = getDatePickerItem()
-    }
-    
-    private func getAddTrackerButtonItem() -> UIBarButtonItem {
-        let barButtonItem = UIBarButtonItem()
-        barButtonItem.image = UIImage(named: "Add tracker icon")
-        barButtonItem.tintColor = .black
-        barButtonItem.action = #selector(addTracker)
-        return barButtonItem
-    }
-    
-    private func getDatePickerItem() -> UIBarButtonItem {
-        let datePicker = UIDatePicker()
-        datePicker.preferredDatePickerStyle = .compact
-        datePicker.datePickerMode = .date
-        
-        let barButtonItem = UIBarButtonItem(customView: datePicker)
-        return barButtonItem
+        navigationItem.leftBarButtonItem = barButtonItem
+        navigationItem.rightBarButtonItem = datePickerItem
     }
     
     private func setupSearchTextField() {
-        let searchTextField = UISearchTextField()
-        searchTextField.placeholder = "Поиск"
-        searchTextField.backgroundColor = UIColor(red: 118, green: 118, blue: 128, alpha: 0.12)
         searchTextField.delegate = self
-        searchTextField.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(searchTextField)
         
         NSLayoutConstraint.activate([
             searchTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             searchTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
             searchTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16)
+        ])
+    }
+    
+    private func setupStarImage() {
+        view.addSubview(imageView)
+        
+        NSLayoutConstraint.activate([
+            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+    
+    private func setupLabel() {
+        view.addSubview(label)
+        
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 8),
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ])
     }
     
